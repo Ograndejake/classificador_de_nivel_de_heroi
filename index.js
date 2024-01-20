50090ac0dd152ebaf6d4a08d0d3a7bf1f3603d3b
Raffael Vairo <99839891+Ograndejake@users.noreply.github.com>
2024-01-20 18:53:25 +0000

    Desafio commitado

---
+++ index.js
@@ -0,0 +1,21 @@
+//variaveis
+let nomeHeroi = "Raffa";
+let xpHeroi = 5500;
+
+if (xpHeroi <= 1000) {
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de Ferro");
+} else if (xpHeroi <= 2000) {
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de Bronze");
+} else if (xpHeroi <= 5000) {
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de Prata");
+} else if (xpHeroi <= 7000) {
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de Ouro");
+} else if (xpHeroi <= 8000) {
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de Platina");
+} else if (xpHeroi <= 9000) {
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de Ascendente");
+} else if (xpHeroi <= 10000) {
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de Imortal");
+} else {
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de Radiante");
+}
